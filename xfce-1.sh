34123520f4ec418b23e3985ddce82af3408246e6
EraySP909 <erayunsurorluu@gmail.com>
2018-12-03 13:16:46 +0100

    Version 1.0
    Base release.
    Comes with Xenial and XFCE.

---
+++ xfce-1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if ! [ $(id -u) = 0 ]; then
+   echo "Run as root please!"
+   exit 1
+fi
+echo "Getting crouton script."
+mkdir AutoCrouton/
+cd AutoCrouton/
+curl -O https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton
+echo "Got the script! It's located in */AutoCrouton/"
+    read -p "Do you wish to have default apps installed? (ca. 900mb) [y/N]" yn
+    case $yn in
+        [Yy]* ) echo "Extras selected. Cancel within 5 seconds if you change your mind. [CTRL+C]";sleep 5; sh crouton -t xfce,xfce-desktop -r xenial;;
+        [Nn]* ) echo "No extras selected. Cancel within 5 seconds if you change your mind. [CTRL+C]"; sleep 5; sh crouton -t xfce -r xenial;;
+        * ) echo "No extras selected. Cancel within 5 seconds if you change your mind. [CTRL+C]"; sleep 5; sh crouton -t xfce -r xenial;
+    esac
+exit
