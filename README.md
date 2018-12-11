6fa8ab3d041a6970489ef657da4e1c0953a8cb83
EraySP909 <erayunsurorluu@gmail.com>
2018-12-11 23:24:04 +0100

    Updated script

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # AutoCrouton
 An automated script for Crouton with Xenial XFCE
+
+##Usage:
+
+Download it over at www.github.com/EraySP909/AutoCrouton/releases
+or
+`wget raw.githubusercontent.com/EraySP909/AutoCrouton/master/xfce-1.sh`
+`curl -L raw.githubusercontent.com/EraySP909/AutoCrouton/master/xfce-1.sh --output xfce-1.sh`
+
+Enter the terminal and execute the script with `sudo sh xfce-1.sh`.
+
+Please report bugs if found.
--- xfce-1.sh
+++ xfce-1.sh
@@ -3,9 +3,12 @@ if ! [ $(id -u) = 0 ]; then
    echo "Run as root please!"
    exit 1
 fi
-echo "Getting crouton script."
+echo "Travelling to a safe location (~/Downloads/)"
+cd ~/Downloads/
+echo "Making a temporary directory."
 mkdir AutoCrouton/
 cd AutoCrouton/
+echo "Getting crouton script."
 curl -O https://raw.githubusercontent.com/dnschneid/crouton/master/installer/crouton
 echo "Got the script! It's located in */AutoCrouton/"
     read -p "Do you wish to have default apps installed? (ca. 900mb) [y/N]" yn
@@ -14,4 +17,6 @@ echo "Got the script! It's located in */AutoCrouton/"
         [Nn]* ) echo "No extras selected. Cancel within 5 seconds if you change your mind. [CTRL+C]"; sleep 5; sh crouton -t xfce -r xenial;;
         * ) echo "No extras selected. Cancel within 5 seconds if you change your mind. [CTRL+C]"; sleep 5; sh crouton -t xfce -r xenial;
     esac
+echo "Removing temporary directory."
+rm -rf ../AutoCrouton
 exit
